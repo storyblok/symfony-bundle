60ea31fab4c1ddf3e1f42d89ce8ee8939b2f574b
Silas Joisten <silasjoisten@proton.me>
2025-04-20 11:59:55 +0300

    Introduce `Route` enum (#27)

---
+++ tests/Unit/Routing/RouteTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of sensiolabs-de/storyblok-bundle.
+ *
+ * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Storyblok\Bundle\Tests\Unit\Routing;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+use Storyblok\Bundle\Routing\Route;
+
+final class RouteTest extends TestCase
+{
+    #[Test]
+    public function constants(): void
+    {
+        self::assertSame('storyblok_webhook', Route::WEBHOOK);
+    }
+
+    #[Test]
+    public function numberOfConstants(): void
+    {
+        $reflection = new \ReflectionClass(Route::class);
+
+        self::assertCount(1, $reflection->getConstants());
+    }
+}
