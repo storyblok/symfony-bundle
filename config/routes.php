e7dbf34643ed1471ac85890246d7a100813de27f
Silas Joisten <silasjoisten@proton.me>
2025-04-20 11:57:58 +0300

    Introduce `Route` enum

--- config/routes.php
+++ config/routes.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Symfony\Component\Routing\Loader\Configurator;
 
 use Storyblok\Bundle\Controller\WebhookController;
+use Storyblok\Bundle\Routing\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 return function (RoutingConfigurator $routes): void {
-    $routes->add('storyblok_webhook', '/webhook/storyblok')
+    $routes->add(Route::WEBHOOK, '/webhook/storyblok')
         ->controller(WebhookController::class)
         ->methods([Request::METHOD_POST])
         ->options([
---
+++ src/Routing/Route.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of sensiolabs-de/storyblok-bundle.
+ *
+ * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Storyblok\Bundle\Routing;
+
+enum Route
+{
+    public const string WEBHOOK = 'storyblok_webhook';
+}
