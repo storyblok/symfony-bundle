32981e7677818a7475f283cd7b7bd4dc69f69310
Silas Joisten <silasjoisten@proton.me>
2025-05-06 10:28:58 +0300

    feat(routing): Split route files

---
+++ config/routes/content_type.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Routing\Loader\Configurator;
+
+use Storyblok\Bundle\ContentType\Listener\ResolveControllerListener;
+use Storyblok\Bundle\Routing\Route;
+
+return function (RoutingConfigurator $routes): void {
+    $routes->add(Route::CONTENT_TYPE, '/{!slug}')
+        /**
+         * Symfony enforces a callable as string here in order to register a route.
+         */
+        ->controller(\sprintf('%s::noop', ResolveControllerListener::class))
+        ->requirements([
+            /**
+             *  \p{L} → Matches any letter (Latin, Kanji, Hiragana, Katakana, etc.)
+             *  \p{N} → Matches any number (so numbers remain valid in slugs)
+             *  (?:-[\p{L}\p{N}]+)* → Allows hyphenated words (e.g., hello-world)
+             *  (?:\/[\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*)*\/? → Allows slashes for hierarchical paths
+             *  Trailing slash (\/?) → Optional to allow both /slug and /slug/.
+             */
+            'slug' => '([\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*(?:\/[\p{L}\p{N}]+(?:-[\p{L}\p{N}]+)*)*\/?)$',
+        ])
+        ->options([
+            'priority' => -10000,
+        ]);
+};
