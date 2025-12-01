cac4cc4173c70b3142df3071a006eeb55caa96d6
Silas Joisten <silasjoisten@proton.me>
2025-12-01 10:16:42 +0100

    dep(symfony): Allow symfony 8 (#77)

--- src/ContentType/Listener/StoryNotFoundExceptionListener.php
+++ src/ContentType/Listener/StoryNotFoundExceptionListener.php
@@ -47,11 +47,11 @@ final readonly class StoryNotFoundExceptionListener
 
         $request = $event->getRequest();
 
-        if ($request->get('_route') !== Route::CONTENT_TYPE) {
+        if ($request->attributes->get('_route') !== Route::CONTENT_TYPE) {
             return;
         }
 
-        $params = $request->get('_route_params', []);
+        $params = $request->attributes->get('_route_params', []);
 
         if (!\array_key_exists('slug', $params)) {
             return;
