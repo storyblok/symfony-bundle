b3e8e92bc39bf659efa3137a9c01579307de7b75
Silas Joisten <silasjoisten@proton.me>
2026-01-15 17:21:18 +0200

    Fix

--- src/Block/Renderer/RendererInterface.php
+++ src/Block/Renderer/RendererInterface.php
@@ -20,6 +20,7 @@ interface RendererInterface
      * @deprecated since version 1.10, to be changed in 2.0 to "render(array|object $values, array $context = []): string".
      *
      * @param array<string, mixed>|object $values The values of the block coming from Storyblok
+     * @param array<string, mixed> $context Additional context to pass to the template
      *
      * @return string Returns HTML
      */
