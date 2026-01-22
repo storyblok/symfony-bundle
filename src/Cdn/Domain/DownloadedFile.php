93c15c76053c71f0d9780cf4af03e5f87c705024
Silas Joisten <silasjoisten@proton.me>
2026-01-22 18:00:52 +0200

    fix

--- src/Cdn/Domain/DownloadedFile.php
+++ src/Cdn/Domain/DownloadedFile.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of storyblok/symfony-bundle.
+ *
+ * (c) Storyblok GmbH <info@storyblok.com>
+ * in cooperation with SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Storyblok\Bundle\Cdn\Domain;
 
 final readonly class DownloadedFile
